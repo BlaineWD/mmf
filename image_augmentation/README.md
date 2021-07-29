56bdc6b15dd155a78fa375cdb98a149ee51151a7
Blaine Dietrich <blaine.dietrich@gmail.com>
2021-07-29 01:34:07 -0400

    tqdm

--- image_augmentation/README.md
+++ image_augmentation/README.md
@@ -6,6 +6,10 @@ Create conda environment: `conda env create -f requirements.yml`
 
 Activate conda environment: `conda activate image_augmentation`
 
-Run script and replace all images with randomly augmented versions of images: `python randomly_augment_images.py`
+Run these from project root (up a level):
 
-Run script and replace 50% of images with randomly augmented versions of images: `python randomly_augment_images.py --replace_ratio 0.5`
+Run script and replace all images with randomly augmented versions of images: `python image_augmentation/randomly_augment_images.py`
+
+Run script and replace 50% of images with randomly augmented versions of images: `python image_augmentation/randomly_augment_images.py --replace_ratio 0.5`
+
+You can also use `--input_path` and `--output_path` to set image folder paths
--- image_augmentation/randomly_augment_images.py
+++ image_augmentation/randomly_augment_images.py
@@ -4,13 +4,20 @@ import matplotlib.pyplot as plt
 import os
 import argparse
 import random
+import shutil
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--input_path', type=str, default='hateful_memes/data/img', help='Input path of unaltered hateful memes dataset')
+parser.add_argument('--output_path', type=str, default='hateful_memes_augmented/data/img', help='Output path of augmented (altered) hateful memes dataset')
 parser.add_argument('--replace_ratio', type=float, help='What proportion of hateful_memes images to replace with randomly augmented versions of images')
 
 args = parser.parse_args()
 replace_ratio = args.replace_ratio
-print(replace_ratio)
+input_path = args.input_path
+output_path = args.output_path
+
+print(f'Replace ratio is {replace_ratio}')
 
 transform = A.Compose([
     A.CLAHE(),
@@ -24,18 +31,26 @@ transform = A.Compose([
     A.HueSaturationValue()
 ])
 
-image_input_path = '../../Downloads/hateful_memes/data/img'
-augmented_image_output_path = 'hateful_memes_augmented/data/img'
-if not os.path.exists(augmented_image_output_path):
-    os.makedirs(augmented_image_output_path)
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
 
-DEV_LIMIT = 10
-for _, _, files in os.walk(image_input_path):
+print(f'Creating augmented dataset from {input_path} and saving to {output_path}...')
+for _, _, files in os.walk(input_path):
+    files = files[:100]
+    # Files to augment is an array of file names that we will apply transform to
+    # All files in files array not in files_to_augment array will be copied without augmentation
     if replace_ratio is not None:
-        files = random.sample(files, replace_ratio * len(files))
-    for file_name in files:
-        image = cv2.imread(os.path.join(image_input_path, file_name))
+        files_to_augment = random.sample(files, replace_ratio * len(files))
+    else:
+        files_to_augment = files.copy()
 
-        augmented_image = transform(image=image)['image']
+    for file_name in tqdm(files):
+        file_input_path = os.path.join(input_path, file_name)
+        file_output_path = os.path.join(output_path, file_name)
 
-        cv2.imwrite(os.path.join(augmented_image_output_path, file_name), augmented_image)
+        if file_name in files_to_augment:
+            image = cv2.imread(file_input_path)
+            augmented_image = transform(image=image)['image']
+            cv2.imwrite(file_output_path, augmented_image)
+        else:
+            shutil.copy(file_input_path, file_output_path)

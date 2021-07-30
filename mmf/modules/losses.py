4e63a7c6f4f9ba34f2e312fcdcddcf2804a067a3
Xiang Fu <xfu34@gatech.edu>
2021-07-30 16:13:40 -0400

    add focal loss function

--- mmf/modules/losses.py
+++ mmf/modules/losses.py
@@ -583,6 +583,24 @@ class CrossEntropyLoss(nn.Module):
     def forward(self, sample_list, model_output):
         return self.loss_fn(model_output["scores"], sample_list["targets"])
 
+@registry.register_loss("focal_loss")
+class FocalLoss(nn.Module):
+    def __init__(self, weight=None,
+                 gamma=2., reduction='none'):
+        nn.Module.__init__(self)
+        self.weight = weight
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, input_tensor, target_tensor):
+        log_prob = nn.functional.log_softmax(input_tensor, dim=-1)
+        prob = torch.exp(log_prob)
+        return nn.functional.nll_loss(
+            ((1 - prob) ** self.gamma) * log_prob,
+            target_tensor,
+            weight=self.weight,
+            reduction=self.reduction
+        )
 
 @registry.register_loss("soft_label_cross_entropy")
 class SoftLabelCrossEntropyLoss(nn.Module):

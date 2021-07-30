7ae1cc7a0f88c21b2e01184d43ea3d153a98db81
Blaine Dietrich <blaine.dietrich@gmail.com>
2021-07-30 00:31:10 -0400

    Now plotting

--- plotting_utils/generate_plots.py
+++ plotting_utils/generate_plots.py
@@ -2,6 +2,8 @@ import argparse
 import os
 import sys
 import json
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_path', type=str, default='experiment_logs', help='Input path to folder of log files from 12 baseline models')
@@ -15,7 +17,6 @@ if not os.path.exists(input_path):
     print(f'Input path {input_path} does not exist, please make sure you are pointing to a folder containing experiment logs')
     sys.exit()
 
-print(f'Reading in experiment logs from {input_path} and saving plots to {output_path}...')
 
 if not os.path.exists(output_path):
     os.makedirs(output_path)
@@ -28,7 +29,9 @@ logistics_line_start = 'mmf.trainers.callbacks.logistics : {'
 
 log_files = os.listdir(input_path)
 metrics = {}
-for log_file in log_files:
+epoch_step = None
+print(f'Reading in experiment logs from {input_path}...')
+for log_file in tqdm(log_files):
     logistics_lines = []
     with open(os.path.join(input_path, log_file)) as f:
         log_lines = f.readlines()
@@ -43,16 +46,30 @@ for log_file in log_files:
     for logistics_line in logistics_lines:
         if 'Finished run' in logistics_line:
             continue
+
         current_metrics_string = logistics_line[logistics_line.find(logistics_line_start) + len(logistics_line_start) - 1:]
         current_metrics = json.loads(current_metrics_string)
-
         progress = current_metrics['progress']
-        epoch_num = progress[:progress.find('/')]
-        if train_roc_auc in current_metrics:
-            metrics[config_file]['train'].append(current_metrics[train_roc_auc])
-        elif validation_roc_auc in current_metrics:
-            metrics[config_file]['validation'].append(current_metrics[validation_roc_auc])
-        elif test_roc_auc in current_metrics:
-            metrics[config_file]['test'].append(current_metrics[test_roc_auc])
 
+        if epoch_step is None:
+            epoch_step = int(progress[:progress.find('/')])
+
+        if train_roc_auc in current_metrics:
+            metrics[config_file]['train'].append(float(current_metrics[train_roc_auc]))
+        elif validation_roc_auc in current_metrics:
+            metrics[config_file]['validation'].append(float(current_metrics[validation_roc_auc]))
+        elif test_roc_auc in current_metrics:
+            metrics[config_file]['test'].append(float(current_metrics[test_roc_auc]))
+
+print('\nFound metrics:')
 print(metrics)
+
+print(f'\nWriting plots to {output_path}...')
+for key in tqdm(metrics.keys()):
+    train_metrics = metrics[key]['train']
+    plt.plot([(i + 1) * epoch_step for i in range(len(train_metrics))], train_metrics)
+    plt.title(f'ROC AUC for {key}')
+    plt.xlabel('Epoch')
+    plt.ylabel('ROC AUC')
+    plt.savefig(os.path.join(output_path, f'{key.replace("/", "-")}-train-roc-auc.png'))
+    plt.clf()

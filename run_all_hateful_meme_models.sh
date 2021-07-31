b547b800e2118c1e56f10e198f123d5a9ca7a893
Blaine Dietrich <blaine.dietrich@gmail.com>
2021-07-31 10:01:18 -0400

    Removing duplicate command

--- run_all_hateful_meme_models.sh
+++ run_all_hateful_meme_models.sh
@@ -44,17 +44,6 @@ mmf_run config=projects/hateful_memes/configs/late_fusion/defaults.yaml \
     training.log_format=json \
     training.evaluate_metrics=true
 
-mmf_run config=projects/hateful_memes/configs/late_fusion/defaults.yaml \
-    model=late_fusion \
-    dataset=hateful_memes \
-    training.max_updates=5000 \
-    training.log_interval=50 \
-    training.checkpoint_interval=5000 \
-    training.evaluation_interval=500 \
-    training.batch_size=16 \
-    training.log_format=json \
-    training.evaluate_metrics=true
-
 mmf_run config=projects/hateful_memes/configs/mmbt/defaults.yaml \
     model=mmbt \
     dataset=hateful_memes \
